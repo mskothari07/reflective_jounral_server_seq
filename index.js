cd2cb8527d04dd7998ca937ca67c8563facf485d
mskothari07 <kotharimeet07@gmail.com>
2023-02-17 13:10:43 +0530

    Changes in Login regsiter api

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+const cookieParser = require("cookie-parser")
 const express = require("express")
 const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
 
 const db = require('./models')
 
 //Routers
 const authRouter = require("./routes/auth.js")
 app.use("/", authRouter)
-app.use("/register", authRouter)
-app.use("/login",authRouter)
+
 
 const port = process.env.PORT || 8001
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,23 @@
 const express = require("express")
 const router = express.Router()
 const {Users} = require("../models")
+const jwt = require("jsonwebtoken")
 
 // router.get("/", (req,res)=>{
 //     res.send("Hello World")
 // })
 
+
+//register api
 router.post("/register", async (req,res) => {
-    const user = req.body
+    const user = req.body 
     console.log(user);
     await Users.create(user)
     res.json(" Register succes")
+    alert ("User Registered Please login")
 })
 
+//login api
 router.post("/login", async (req,res) =>{
     Users.findOne({
         where:{
@@ -23,12 +28,16 @@ router.post("/login", async (req,res) =>{
         if(!user){
             res.status(401).send("Invalid Email or Password")
         }else{
-            res.json("Login Succes")
+            const token = jwt.sign({id : user.id}, "secretkey")
+            res.cookie("unlock_token", token,{
+                httpsOnly:true,            
+            })
+            .status(200)
+            .json("Login success")
         }
     })   
 })
 
-
-
+//logout
 
 module.exports = router
