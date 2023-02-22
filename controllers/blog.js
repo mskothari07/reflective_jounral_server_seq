96c899313a005a6fca2a8b7e1ef0a24fc681b379
mskothari07 <kotharimeet07@gmail.com>
2023-02-22 15:00:10 +0530

    Update blog, delete blog

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,8 +1,8 @@
 const { Blogs } = require("../models");
 const jwt = require("jsonwebtoken");
 
-//Fetching all Blogs
 
+//Fetching all Blogs
 const getBlogs = async (req, res) => {
   try {
     const { cat } = req.query;
@@ -17,7 +17,6 @@ const getBlogs = async (req, res) => {
 };
 
 //adding Blog
-
 const addblog = async (req, res) => {
   const token = req.cookies.unlock_token;
   if (!token) return res.status(404).json("No token Found");
@@ -30,7 +29,7 @@ const addblog = async (req, res) => {
       img: req.body.img,
       cat: req.body.cat,
       date: req.body.date,
-      userId: userInfo.id,
+      userid: userInfo.id,
     });
     return res.json("Blog added");
   } catch (err) {
@@ -42,7 +41,6 @@ const addblog = async (req, res) => {
 };
 
 //getting single blog
-
 const getBlog = async (req, res) => {
   try {
     const blog = await Blogs.findOne({
@@ -55,4 +53,79 @@ const getBlog = async (req, res) => {
   }
 };
 
-module.exports = { getBlogs, addblog, getBlog };
+//update blog
+const updateBlog = async(req,res) =>{
+    const token = req.cookies.unlock_token
+    if(!token) return res.status(401).json("No token FOund")
+
+    try{
+        const userInfo = jwt.verify(token, "jwtkey")
+        const blogId = req.params.id
+
+        const updatedBlog = await Blogs.update(
+            {
+                title:req.body.title,
+                desc:req.body.desc,
+                img:req.body.img,
+                cat:req.body.cat,
+            },
+            {
+                where :{
+                    id : blogId,
+                    userid: userInfo.id
+                }
+            }
+        )
+        if (updatedBlog[0] === 0){
+            return res.status(404).json("Blog not found or unauthorized")
+        }
+        return res.json('blog updated')
+    } catch (err){
+        return res.status(500).json(err)
+    }
+}
+
+
+//Deleting Blog
+const deleteBlog = (req,res) =>{
+    //Token extract from cookies
+
+    const token = req.cookies.unlock_token
+    if(!token) {
+        res.status(401).json("No token")
+    }
+
+    //token verfication and getting userinfo
+    jwt.verify(token, 'jwtkey', (err,userInfo) =>{
+        if (err) {
+            return res.status(404).json('Invalid token')
+        }
+
+
+    //Finding blog to delete
+    const blogId = req.params.id
+    Blogs.findOne({
+        where:{
+            id : blogId,
+            userid : userInfo.id
+        },
+    })
+        .then((Blogs) =>{
+            if(!Blogs){
+                return res.status(404).json('you do have access to delete it')
+            }
+
+            //Delete the blog
+            Blogs.destroy().then(()=>{
+                return res.json('DELETED')
+            })
+        })
+        .catch((err)=>{
+            return res.status(500).json('Some Error occcured')
+        })
+    })
+} 
+
+
+
+module.exports = { getBlogs, addblog, getBlog, updateBlog, deleteBlog };
--- routes/blog.js
+++ routes/blog.js
@@ -1,22 +1,21 @@
 const express = require("express")
 const router = express.Router()
 
-const {getBlogs,addblog,getBlog} = require("../controllers/blog.js")
+const {getBlogs,addblog,getBlog, updateBlog, deleteBlog} = require("../controllers/blog.js")
 
 //Home Page Getting all blogs
 router.get("/", getBlogs)
 
-
-// //Single Page i.e getting single blog
+//Single Page i.e getting single blog
 router.get("/:id",getBlog)
 
-// //Adding a Blog
+//Adding a Blog
 router.post("/",addblog)
 
-// //Delete Blog
-// router.delete("/:id", deleteblog)
+//Delete Blog
+router.delete("/:id", deleteBlog)
 
-// //Update Blog
-// router.put("/:id",updateblog)
+//Update Blog
+router.put("/:id",updateBlog)
 
 module.exports = router ;
